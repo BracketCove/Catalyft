eae035a1b70b4930774a480991e3a8730dd1255a
r_kay <rkay301@gmail.com>
2018-08-20 09:47:12 -0700

    non building properly, prior to temporarily reverting to single module

--- app/src/test/java/com/wiseassblog/catalyft/WorkoutTestData.kt
+++ app/src/test/java/com/wiseassblog/catalyft/WorkoutTestData.kt
@@ -9,7 +9,7 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class WorkoutTestData {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
